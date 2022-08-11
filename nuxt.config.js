b0bbd49a896525d90cf238c7eb92ae1f10a2be11
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-08-11 15:10:39 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "./static/favicon.ico" },
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css?family=Oswald|Libre+Baskerville&display=swap",
